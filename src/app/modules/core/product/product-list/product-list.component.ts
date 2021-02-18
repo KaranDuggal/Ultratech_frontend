dc49cb4cbcb2a68372fa46cf20411a8c0bfdee71
KaranDuggal <duggalkaran400@gmail.com>
2021-02-18 18:43:24 +0530

    working on product

--- src/app/modules/core/product/product-list/product-list.component.ts
+++ src/app/modules/core/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from 'src/app/services/api/api.service'
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  productArr:any
+  constructor(
+    private apiService: ApiService,
+  ) { }
 
   ngOnInit(): void {
+    this.getdata()
+  }
+  getdata(){
+    this.apiService.callAPI("get","", "api/products/allproduct").subscribe((data:any) => {
+      this.productArr = data.data
+      console.log('this.productArr', this.productArr)
+    })
   }
 
 }
--- src/app/modules/core/product/product-routing.module.ts
+++ src/app/modules/core/product/product-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEditProductComponent } from './add-edit-product/add-edit-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   {
     path:'addproduct',
     component:AddEditProductComponent
+  },
+  {
+    path:'allproduct',
+    component:ProductListComponent
   }
 ];
 
