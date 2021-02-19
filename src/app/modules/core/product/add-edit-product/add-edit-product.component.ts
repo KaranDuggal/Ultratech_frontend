3d1172a93ffb7b6880b46a7f6d4b8e55a55b2628
KaranDuggal <duggalkaran400@gmail.com>
2021-02-19 17:14:55 +0530

    working on add edit comp

--- src/app/modules/core/product/add-edit-product/add-edit-product.component.ts
+++ src/app/modules/core/product/add-edit-product/add-edit-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormGroupName, Validators } from '@angular/forms'
-import {ApiService} from 'src/app/services/api/api.service'
+import { ApiService } from 'src/app/services/api/api.service'
+import { Router, ActivatedRoute, Params } from '@angular/router'
+
 @Component({
   selector: 'app-add-edit-product',
   templateUrl: './add-edit-product.component.html',
@@ -8,14 +10,17 @@ import {ApiService} from 'src/app/services/api/api.service'
 })
 export class AddEditProductComponent implements OnInit {
 
-  // @ViewChild('file1',{static:false})
   images: any
-  multipleImages=[]
-  
+  showme: boolean = false
+  multipleImages = []
+  data = []
+  // @ViewChild("product_type") product_type:ElementRef
+
   constructor(
     private apiService: ApiService,
     private formBuilder: FormBuilder,
-    // private elementRef :ElementRef
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) { }
   addproductform = this.formBuilder.group({
     product_name: new FormControl(null, { validators: [Validators.required] }),
@@ -23,7 +28,7 @@ export class AddEditProductComponent implements OnInit {
     brand_name: new FormControl(null, { validators: [Validators.required] }),
     size: new FormControl(null, { validators: [Validators.required] }),
     quantity: new FormControl(null, { validators: [Validators.required] }),
-    price: new FormControl(null, { validators: [Validators.required] })
+    price: new FormControl(null, { validators: [Validators.required, Validators.minLength(8)] })
     // images: this.formBuilder.array([
     //   new FormControl(null, { validators: [Validators.required] }),
     //   new FormControl(null, { validators: [Validators.required] }),
@@ -31,18 +36,13 @@ export class AddEditProductComponent implements OnInit {
     //   new FormControl(null, { validators: [Validators.required] }),
     // ])
   })
-
-  get checkaddproduct() {
-    return this.addproductform.controls
-  }
+  get checkaddproduct() { return this.addproductform.controls }
   productdata() {
     const formData = new FormData()
     for (const img of this.multipleImages) {
-      formData.append('images',img)
+      formData.append('images', img)
     }
-    formData.append("data",this.addproductform.value)
-    // console.log(formData);
-    // const image = elementRef.
+    // formData.set(username,"karan")
     this.apiService.callAPI("post", formData, "api/products/addproduct").subscribe((data) => {
       console.log('==================');
       console.log('data===> ', data)
@@ -51,10 +51,51 @@ export class AddEditProductComponent implements OnInit {
     console.log(this.images);
   }
   ngOnInit(): void {
+    // this.activatedRoute.params.subscribe((params: any) => { if (params.id) { this.isAddMode = false } })
   }
-  onFileChange(event:any){
-    if(event.target.files.length>0){
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
       this.multipleImages = event.target.files
     }
+  }
+
+
+  // ===================field conditions ==================
+  UWPS_value: boolean = false
+  KNXT_value: boolean = false
+  size: boolean = false
+  quantity: boolean = false
+  product_type(data: any) {
+    if (data === "Cement") {
+      this.UWPS_value = true
+      this.quantity = true
+      this.KNXT_value = false
+      this.size = false
+
+    }
+    if (data === "Steel") {
+      this.UWPS_value = false
+      this.quantity = false
+      this.KNXT_value = true
+      this.size = true
+    }
+  }
+  onkeydown(e: any) {
+    console.log(e.length);
+    let input;
+    if (e.metaKey || e.ctrlKey) {
+      return true;
+    }
+    if (e.which === 32) {
+      return false;
+    }
+    if (e.which === 0) {
+      return true;
+    }
+    if (e.which < 33) {
+      return true;
+    }
+    input = String.fromCharCode(e.which);
+    return !!/[\d\s]/.test(input);
   }
 }
--- src/app/modules/core/product/product-details/product-details.component.ts
+++ src/app/modules/core/product/product-details/product-details.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService} from 'src/app/services/api/api.service'
-import {SweetalertService} from 'src/app/services/alert/sweetalert.service'
-import {Router,ActivatedRoute,Params} from '@angular/router'
+import { ApiService } from 'src/app/services/api/api.service'
+import { SweetalertService } from 'src/app/services/alert/sweetalert.service'
+import { Router, ActivatedRoute, Params } from '@angular/router'
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  productData:any
+  productData: any;
   constructor(
     private apiService: ApiService,
-    private alert:SweetalertService,
-    private router:Router,
-    private activatedRoute:ActivatedRoute
+    private alert: SweetalertService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) { }
-  async getdata(){
+  async getdata() {
     this.activatedRoute.params.subscribe((params: any) => {
       const id = params.id
-      this.apiService.callAPI("get","", `api/products/allproduct/${id}`).subscribe((data:any) => {
+      this.apiService.callAPI("get", "", `api/products/allproduct/${id}`).subscribe((data: any) => {
         this.productData = data.data
         // console.log('productdata', this.productData.product_name)
       })
-    });   
+    });
   }
 
   ngOnInit(): void {
